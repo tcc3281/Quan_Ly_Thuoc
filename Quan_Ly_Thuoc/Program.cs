1544c9ec30dc976792622618ce1b1cb7a1bab830
tcc3281 <tcc3281@gmail.com>
2023-09-28 16:34:43 +0700

    merge

--- Quan_Ly_Thuoc/Program.cs
+++ Quan_Ly_Thuoc/Program.cs
@@ -14,6 +14,7 @@ namespace Quan_Ly_Thuoc
         [STAThread]
         static void Main()
         {
+            //mai
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());

9067a6df19e106a9c0f4755c75a515e772496f7c
Chung30 <lhongquangtrung@gmail.com>
2023-11-20 12:26:51 +0700

    cho chien

--- Quan_Ly_Thuoc/Data/ProcessDatabase.cs
+++ Quan_Ly_Thuoc/Data/ProcessDatabase.cs
@@ -16,9 +16,9 @@ namespace Quan_Ly_Thuoc.Data
         public SqlDataAdapter adapter;
         public DataSet dset;
 
-        //string conStr = "Data Source=.\\sqlexpress;Initial Catalog=QL_Thuoc;Integrated Security=True";
-        string conStr = "Data Source=DESKTOP-U9CQUB1;Initial Catalog=QL_Thuoc;" +
-            "Integrated Security=True";
+        string conStr = "Data Source=.\\sqlexpress;Initial Catalog=QL_Thuoc;Integrated Security=True";
+        //string conStr = "Data Source=DESKTOP-U9CQUB1;Initial Catalog=QL_Thuoc;" +
+        //    "Integrated Security=True";
 
         public ProcessDatabase()
         {
